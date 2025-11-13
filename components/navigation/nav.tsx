f21ba380f75618f46276315da3ffc376155baeb1
PauliusJaras <38277493+PauliusJaras@users.noreply.github.com>
2025-11-13 15:33:23 +0200

    updating styling for navigation components

--- components/navigation/nav.tsx
+++ components/navigation/nav.tsx
@@ -4,25 +4,25 @@ import MenuOpenButton from "./menuOpenButton";
 
 export default function Nav() {
   return (
-    <nav className="fixed top-[5vh] left-1/2 -translate-x-1/2 flex gap-3 justify-center items-center bg-[#090A09] p-3 uppercase rounded-xl transition-all shadow-md text-sm">
+    <nav className="fixed top-[8vh] lg:left-1/12 lg:-translate-x-1/12 left-1/2 -translate-x-1/2 flex gap-2 justify-center items-center bg-[#090A09] p-2 uppercase rounded-xl transition-all shadow-md text-sm">
       <MenuOpenButton />
       <Link href={"/"}>
         <Image src={"/logo.svg"} alt="Saknys logo" width={99} height={350} />
       </Link>
       <Link
-        className="hover:bg-current/5 hover:border-current/15 border-transparent border rounded-lg py-2 px-4 text-md transition-all duration-500 ease-out-in"
+        className="hover:bg-current/5 hover:border-current/15 border-transparent border rounded-lg py-3 px-4 text-md transition-all duration-500 ease-out-in"
         href={"/blog"}
       >
         blog
       </Link>
       <Link
-        className="hover:bg-current/5 hover:border-current/15 border-transparent border rounded-lg py-2 px-4 text-md transition-all duration-500 ease-out-in"
+        className="hover:bg-current/5 hover:border-current/15 border-transparent border rounded-lg py-3 px-4 text-md transition-all duration-500 ease-out-in"
         href={"/about"}
       >
         about
       </Link>
       <Link
-        className="bg-current/5 hover:bg-current/10 hover:border-current/25 rounded-lg py-3.5 px-4 text-md border border-current/15 transition-all duration-500 ease-out-in"
+        className="bg-current/5 hover:bg-current/10 hover:border-current/25 rounded-lg py-3 px-4 text-md border border-current/15 transition-all duration-500 ease-out-in"
         href={"/"}
       >
         Užsisakyti
