7c37696e3dda45d8a0b783aaddedda93be659ade
PauliusJaras <38277493+PauliusJaras@users.noreply.github.com>
2025-12-02 22:24:13 +0200

    dialog menu animation update

--- components/navigation/menuDialog.tsx
+++ components/navigation/menuDialog.tsx
@@ -1,10 +1,13 @@
 "use client";
 
+import Link from "next/link";
 import FlopText from "../FlopText";
 import MotionCard from "../MotionCard";
-import TextButtonCard from "../TextButtonCard";
 import MenuCloseButton from "./menuCloseButton";
 import { cn } from "@/helpers/cn";
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
+import { useTranslations } from "next-intl";
 
 export default function MenuDialog({
   openMenu,
@@ -13,12 +16,35 @@ export default function MenuDialog({
   openMenu: boolean;
   setOpenMenu: (value: boolean) => void;
 }) {
+  const pathname = usePathname();
+  const t = useTranslations();
+
+  const menuCloseHandler = () => {
+    setOpenMenu(false);
+  };
+
+  useEffect(() => {
+    if (openMenu) {
+      setTimeout(() => setOpenMenu(false), 300);
+    }
+  }, [pathname]);
+
+  const links = [
+    { href: "/about", text: t("Navigation.about") },
+    { href: "/blog", text: t("Navigation.blog") },
+    { href: "/contact", text: t("Navigation.contact") },
+    { href: "/reservation", text: t("Navigation.reservation") },
+    { href: "/", text: t("Navigation.home") },
+  ];
+
   return (
     <div
       className={cn(
-        "w-screen h-screen bg-background absolute z-999 overflow-hidden transition-opacity duration-150 opacity-0 invisible pointer-events-none p-4",
+        "w-screen h-screen bg-background absolute z-999 overflow-hidden transition-opacity duration-500 opacity-0 pointer-events-none p-4",
         {
-          "opacity-100 visible pointer-events-auto": openMenu,
+          "opacity-100 visible pointer-events-auto transition-opacity duration-500":
+            openMenu,
+          "opacity-0": !openMenu,
         }
       )}
     >
@@ -27,25 +53,17 @@ export default function MenuDialog({
           className={cn("absolute top-8 left-8", {
             hidden: !openMenu,
           })}
-          onClick={() => setOpenMenu(false)}
+          onClick={menuCloseHandler}
         />
         <div className="flex gap-4 flex-col items-center justify-center">
           <div className="size-3 rotate-45 border border-current/15"></div>
-          <MotionCard>
-            <FlopText text="test" />
-          </MotionCard>
-          <MotionCard>
-            <FlopText text="test" />
-          </MotionCard>
-          <MotionCard>
-            <FlopText text="test" />
-          </MotionCard>
-          <MotionCard>
-            <FlopText text="test" />
-          </MotionCard>
-          <MotionCard>
-            <FlopText text="test" />
-          </MotionCard>
+          {links?.map((link, index) => (
+            <Link key={index} href={link.href}>
+              <MotionCard>
+                <FlopText text={link.text} />
+              </MotionCard>
+            </Link>
+          ))}
           <div className="size-3 rotate-45 border border-current/15"></div>
         </div>
       </div>
