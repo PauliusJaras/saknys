7a7af3b81c888508ccc7c7bbdd6176c60e1024c8
PauliusJaras <38277493+PauliusJaras@users.noreply.github.com>
2025-12-12 14:59:19 +0200

    updating all pages to have curved cards

--- app/[locale]/reservation/page.tsx
+++ app/[locale]/reservation/page.tsx
@@ -1,5 +1,32 @@
 export const dynamic = "force-static";
 
+import CurvedCard from "@/components/CurvedCard";
+import { StaggeredFadeBlur } from "@/components/hero/StagerredFade";
+import { getTranslations } from "next-intl/server";
+import Image from "next/image";
+
 export default async function ReservationPage() {
-  return <div className="">Reservation</div>;
+  const t = await getTranslations();
+  return (
+    <div className="grid grid-cols-12 h-full gap-4">
+      <CurvedCard
+        className="col-span-6 min-h-[400px] md:min-h-40"
+        hideAction={true}
+      >
+        <Image
+          alt={"Image placeholder"}
+          fill
+          className="object-cover w-full h-full overflow-hidden"
+          src={"/images/placeholder2.jpg"}
+        ></Image>
+        <div className="z-50 absolute bottom-14 left-1/2 md:left-14 -translate-x-1/2 md:translate-x-0">
+          <StaggeredFadeBlur
+            className="w-[10ch] text-center md:text-left text-6xl md:text-8xl leading-20 md:leading-30 font-bodoni"
+            text={t("Navigation.reservation")}
+          />
+        </div>
+      </CurvedCard>
+      <div className="col-span-6">reservation</div>
+    </div>
+  );
 }
